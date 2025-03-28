cec41c8e09e34c3fc816e46867d9558a57d8fdf5
User <example@example.com>
2025-03-28 15:56:29 -0500

    Fix: Add proper ESM-compatible Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  testMatch: ['<rootDir>/src/**/*.test.(ts|tsx)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      useESM: true,
+    }]
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx']
+};
